86f66066b740e7ca0b2a7350157a2c310540948e
Alonso Ortega <alonsojortega@gmail.com>
2019-02-09 05:22:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,14 +13,6 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # library = load_library(file_path)
-  # result = library["get_emoticon"][emoticon]
-  #
-  # if result
-  #   result
-  # else
-  #   "Sorry, that emoticon was not found"
-  # end
 
 end
 
